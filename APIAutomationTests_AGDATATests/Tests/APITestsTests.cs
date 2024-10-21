b49f9e06f64d59772440b3dd35164d584facf081
Naga Santhosh Kesana <santhoshgtr35@gmail.com>
2024-10-21 16:34:23 -0400

    ApiTests Added

---
+++ APIAutomationTests_AGDATATests/Tests/APITestsTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AGDATA_APIAutomationTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGDATA_APIAutomationTests.Tests
+{
+    [TestClass()]
+    public class APITestsTests
+    {
+        [TestMethod()]
+        public void APITestsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUser_ShouldReturnValidUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostUser_ShouldReturnValidUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutUser_ShouldReturnValidUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUser_ShouldReturnValidUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostUserComments_ShouldReturnValidUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserWithId_ShouldReturnValidUserTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
