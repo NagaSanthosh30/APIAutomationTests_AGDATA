d0e011785bb2a64d91ada0e7a9a206c89cad317e
Naga Santhosh Kesana <santhoshgtr35@gmail.com>
2024-10-21 15:01:50 -0400

    Configuration and Schema Validation class file added

---
+++ APIAutomationTests_AGDATA/Helpers/ConfigurationLoader.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace AGDATA_APIAutomationTests.Helpers
+{
+    public class Configuration
+    {
+        public string BaseUrl { get; set; }
+        public string AuthToken { get; set; }
+    }
+
+    public static class ConfigurationLoader
+    {
+        public static Configuration LoadConfig()
+        {
+            var configFilePath = Directory.GetCurrentDirectory()+"\\config.json";
+            var json = File.ReadAllText(configFilePath);
+            return JsonConvert.DeserializeObject<Configuration>(json);
+        }
+    }
+
+}
---
+++ APIAutomationTests_AGDATA/Helpers/SchemaValidator.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace AGDATA_APIAutomationTests.Helpers
+{
+    public class SchemaValidator
+    {
+        public bool ValidateSchema(string jsonResponse, string jsonSchema)
+        {
+            JSchema schema = JSchema.Parse(jsonSchema);
+
+            try
+            {
+                if (jsonResponse.Trim().StartsWith("["))
+                {
+                    // If the JSON is an array
+                    JArray responseArray = JArray.Parse(jsonResponse);
+                    return responseArray.IsValid(schema, out IList<string> errorMessages);
+                }
+                else
+                {
+                    // If the JSON is an object
+                    JObject responseObject = JObject.Parse(jsonResponse);
+                    return responseObject.IsValid(schema, out IList<string> errorMessages);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Schema validation error: {ex.Message}");
+                return false;
+            }
+        }
+    
+    }
+}
