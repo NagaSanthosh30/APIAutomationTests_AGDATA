9ce7956219696cd5c69d082d577d1a734e2f6bae
Naga Santhosh Kesana <santhoshgtr35@gmail.com>
2024-10-21 22:07:02 -0400

    PageBase class file added

---
+++ WebUIAutomation_AGDATA/PageBase/Page.cs
@@ -0,0 +1,13 @@
+﻿using AGDATA_WebUIAutomation.PageObjects;
+
+namespace AGDATA_WebUIAutomation.PageBase
+{
+    public class Page
+    {
+        public static ContactPage ContactPage => new ContactPage();
+        public static HomePage HomePage => new HomePage();
+        public static MarketIntelligence MarketIntelligence => new MarketIntelligence();
+        public static BasePage BasePage => new BasePage();
+
+    }
+}
